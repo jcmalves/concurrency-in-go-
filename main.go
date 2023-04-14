679c222eeeec48aac7dde37dacd9abeeb2cea49a
juliocesar1402 <julio.monteiro@mercadolivre.com>
2023-04-14 16:02:01 -0300

    feat: the pizzeria funcion
    getting start with the producer

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"math/rand"
+	"time"
+
+	"github.com/fatih/color"
+)
+
 const NumberOfPizzas = 10
 
 var pizzasMade, PizzasFailed, total int
@@ -15,12 +22,40 @@ type PizzaOrder struct {
 	success     bool
 }
 
+func (p *Producer) Close() error {
+	ch := make(chan error)
+	p.quit <- ch
+	return <-ch
+}
+
+func pizzeria(pizzaMaker *Producer) {
+	// keep track if which pizza we are making
+
+	// run forever or until we receive a quit notification
+
+	// try to make pizzas
+	for {
+		// try to make a pizza
+		// decision
+	}
+}
+
 func main() {
 	// seed the random number generator
+	rand.Seed(time.Now().UnixNano())
 
 	// print out a message
+	color.Cyan("The Pizzeria is open for business!")
+	color.Cyan("----------------------------------")
 
 	// create a producer
+	pizzaJob := &Producer{
+		data: make(chan PizzaOrder),
+		quit: make(chan chan error),
+	}
+
+	// run the producer in the background
+	go pizzeria(pizzaJob)
 
 	// create and run consumer
 
