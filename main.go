345b9d1f073637e7667058862293fda59501ab4d
juliocesar1402 <julio.monteiro@mercadolivre.com>
2023-04-14 13:40:57 -0300

    feat: testing for race conditions

--- main.go
+++ main.go
@@ -8,17 +8,22 @@ import (
 var msg string
 var wg sync.WaitGroup
 
-func updateMessage(s string) {
+func updateMessage(s string, m *sync.Mutex) {
 	defer wg.Done()
+
+	m.Lock()
 	msg = s
+	m.Unlock()
 }
 
 func main() {
 	msg = "Hello, world"
 
+	var mutext sync.Mutex
+
 	wg.Add(2)
-	go updateMessage("Hello, universe!")
-	go updateMessage("Hello, cosmos!")
+	go updateMessage("Hello, universe!", &mutext)
+	go updateMessage("Hello, cosmos!", &mutext)
 	wg.Wait()
 
 	fmt.Println(msg)
